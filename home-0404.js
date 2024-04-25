082e1e10189d656f886accd2de8ab4c7f997f752
Vennnnnnnn <dwzw1122@gmail.com>
2024-04-25 19:37:49 +0800

    renew RWD

--- home-0404.js
+++ home-0404.js
@@ -1,7 +1,10 @@
-let hb = document.querySelector(".header-hb");
-function ClickHb() {
-    
+function toggleMenu() {
+    let nav = document.querySelector('.header-nav');
+    nav.classList.toggle('show');
 }
+
+let hbMenu = document.querySelector('.header-hb');
+hbMenu.addEventListener('click', toggleMenu);
 
 
 
