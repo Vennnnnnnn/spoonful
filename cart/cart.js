00f01e3f281612c744215a34a4396f2072a09e37
Vennnnnnnn <dwzw1122@gmail.com>
2024-04-28 23:28:20 +0800

    trnew RED

--- cart/cart.js
+++ cart/cart.js
@@ -1,19 +1,33 @@
 // let remove = document.querySelectorAll(".remove");
 // remove.addEventListener("click", AddAndRemove);
-function AddAndRemove() {
-    let add = document.querySelectorAll(".add");
-    for (let i = 0; i < add.length; i++) {
-        let nmu = document.querySelectorAll(".cart-card-num");
-        nmu[i].value = parseInt(nmu[i].value)+ 1;
-    }
-    
+let add    = document.querySelectorAll(".add");
+let remove = document.querySelectorAll(".remove");
+let num    = document.querySelectorAll(".cart-card-num");
+let price  = document.querySelectorAll(".cart-card-price");
 
-    // if ()
-}
-let add = document.querySelectorAll(".add");
-for (let i = 0; i < add.length; i++) {
-    
-    add[i].addEventListener("click", AddAndRemove,false);
+let addAndRemove = [add, remove];
+
+for (let i = 0; i < addAndRemove.length; i++) {
+    for (let j = 0; j < addAndRemove[i].length; j++) {
+        if (i === 0) {
+            add[j].addEventListener("click", function() {
+                num[j].value = parseInt(num[j].value) + 1;
+        
+                price[j].innerHTML = `$ ${(parseInt(price[j].innerHTML.trim().split(" ")[1]) / (num[j].value - 1)) * num[j].value}`;
+            });
+        } else {
+            remove[j].addEventListener("click", function() {
+                if (parseInt(num[j].value) <= 1) {
+                    return;
+                }
+                num[j].value = parseInt(num[j].value) - 1;
+                
+                let singleItemPrice = (parseInt(price[j].innerHTML.trim().split(" ")[1]) / (parseInt(num[j].value) + 1));
+        
+                price[j].innerHTML = `$ ${singleItemPrice * num[j].value}`;
+            });
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
--- js/header_hb_click.js
+++ js/header_hb_click.js
@@ -1,5 +1,13 @@
-$(function() {
-    $(".header-icon").click(function() {
-        $(this).slideToggle(1500, 'easeOutBack');
-    })
-})
+// $(function() {
+//     $(".header-icon").click(function() {
+//         $(this).slideToggle(1500, 'easeOutBack');
+//     })
+// })
+
+function toggleMenu() {
+    let nav = document.querySelector('.header-nav');
+    nav.classList.toggle('show');
+}
+
+let hbMenu = document.querySelector('.header-hb');
+hbMenu.addEventListener('click', toggleMenu);
